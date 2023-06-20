9e269593b20325ff2f00f48f9a5fcf0450d8cde0
Oxyhertz <prolonchuk5@gmail.com>
2023-06-20 20:02:38 +0300

    build version

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/feedback-app-vue/sw.js', { scope: '/feedback-app-vue/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
