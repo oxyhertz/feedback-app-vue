8d77a2f0e36082bec9447f00f20833fd74754101
Oxyhertz <prolonchuk5@gmail.com>
2023-06-20 14:13:22 +0300

    Store

--- src/store/store.js
+++ src/store/store.js
@@ -1,23 +1,32 @@
 import Vuex from 'vuex'
-
+import { surveyService } from '../services/survey.service.local'
 export const store = Vuex.createStore({
   strict: true,
   state: {
     userStaus: null,
+    surveys: [],
     currStage: 1,
     statuses: [
-      { id: '314rsafda', cmp: 'HappyFaceIcon', status: 'happy', path: 'msg' },
+      {
+        id: '314rsafda',
+        cmp: 'HappyFaceIcon',
+        status: 'happy',
+        path: 'msg',
+        rate: 3,
+      },
       {
         id: '1324dfcg',
         cmp: 'NeutralFaceIcon',
         status: 'neutral',
         path: 'contact',
+        rate: 2,
       },
       {
         id: '1234dade',
         cmp: 'DissapointedFaceIcon',
         status: 'dissapointed',
         path: 'contact',
+        rate: 1,
       },
     ],
   },
@@ -31,8 +40,18 @@ export const store = Vuex.createStore({
     currStage({ currStage }) {
       return currStage
     },
+    surveys({ surveys }) {
+      const surveysCopy = JSON.parse(JSON.stringify(surveys))
+      return surveysCopy.sort((a, b) => b.createdAt - a.createdAt)
+    },
   },
   mutations: {
+    addSurvey(state, { survey }) {
+      state.surveys.push(survey)
+    },
+    setSurveys(state, { surveys }) {
+      state.surveys = surveys
+    },
     setUserStatus(state, { status }) {
       state.userStaus = status.status
     },
@@ -41,5 +60,28 @@ export const store = Vuex.createStore({
       state.currStage = stage
     },
   },
-  actions: {},
+  actions: {
+    async addSurvey(context, { survey }) {
+      try {
+        survey = await surveyService.save(survey)
+        context.commit({
+          type: 'addSurvey',
+          survey,
+        })
+        return survey
+      } catch (err) {
+        console.log('surveyStore: Error in addSurvey', err)
+        throw err
+      }
+    },
+    async loadSurveys(context) {
+      try {
+        const surveys = await surveyService.query()
+        context.commit({ type: 'setSurveys', surveys })
+      } catch (err) {
+        console.log('surveyStore: Error in loadSurvey', err)
+        throw err
+      }
+    },
+  },
 })
